8ce7b059e573b83154e0ba967fbf31a4c37a1c80
kyrajeep <jwp11@sfu.ca>
2023-01-13 20:56:52 -0800

    Add comments and display a plot

--- supervised_learning/gdp.py
+++ supervised_learning/gdp.py
@@ -1,13 +1,30 @@
-#cd import sklearn
+# Question: Can we predict the GDP of Zimbabwe in 2023 
+# given the GDP of neighboring countries?
+# Are they correlated? 
+# How about for Canada and the US?
+
+# Can try with data from the 1980's to predict 1990's.
+from sklearn import linear_model
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('World GDP Dataset.csv')
 data.rename(columns = {'GDP, current prices (Billions of U.S. dollars)':'GDP (billions USD)'}, inplace = True)
-print(data.columns)
+#print(data.columns)
 
-data1 = data.drop([i for i in range (196, 230)], axis = 0)
-print(data1.head)
-# a smaller dataset with Zimbabwe
-zimbabwe = data1[data1["GDP (billions USD)"] == "Zimbabwe"]
+#Clean out the columns that have no information
+data = data.drop([i for i in range (196, 230)], axis = 0)
+#print(data.head)
+
+# Plot for all countries
+plt.figure(); data.plot(style='k--', label='Series'); plt.legend()
+plt.show()
+
+# Make a smaller dataset with Zimbabwe
+zimbabwe = data[data["GDP (billions USD)"] == "Zimbabwe"]
 print(zimbabwe.iloc[:, [i for i in range(5,19)]], "Zimbabwe recorded their first significant GDP in 1990 with 10.144 billion USD")
+
+model = linear_model.LinearRegression()
+
+
